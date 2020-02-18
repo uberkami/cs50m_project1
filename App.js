1e37094a41d925b2b660df76d57d959a4874189a
uberkami <uberkami@gmail.com>
2020-02-18 13:26:39 +0100

    project1 finished

--- App.js
+++ App.js
@@ -4,16 +4,15 @@ import Constants from 'expo-constants';
 
 let workActive = false;
 let countInterval
-// let startButton = document.getElementById("startButton")
-// let stopButton = document.getElementById("stopButton")
+
 export default class App extends React.Component {
   constructor() {
     super()
     this.myUi = React.createRef()
     this.state = {
-      count: 0,
-      workMinutes: 0,
-      pauseMinutes: 0,
+      count: (25*60),
+      workMinutes: 25,
+      pauseMinutes: 5,
       workTime: true,
     }
   }
@@ -22,27 +21,17 @@ export default class App extends React.Component {
     return Math.floor(this.state.count / 60)
   }
 
+
   secondsLeft() {
     return this.state.count % 60
-  }
-
-
-  setTimer(remainingTime) {
-    console.log('setTimer', remainingTime)
-    this.setState({
-      count: remainingTime * 60
-    }).bind(this)
-    console.log('setTimer ende', remainingTime)
   }
 
 
   countDown() {
     console.log('this.state.count in timeout/intervall', this.state.count)
     this.setState({
-      // count: this.state.count - 1
       count: this.state.count - 1
     })
-    // this.setTimer(this.state.count - 1).bind(this)
     console.log('countDownlog')
     if (this.state.count === 0) {
       Vibration.vibrate(1000)
@@ -50,7 +39,7 @@ export default class App extends React.Component {
       workActive = false
       let work = this.state.workTime
       console.log('countDown this.state.workTime', this.state.workTime)
-      this.setState({workTime: !work})
+      this.setState({ workTime: !work })
       console.log('countDown2 this.state.workTime', this.state.workTime)
       this.startCount()
     }
@@ -58,49 +47,25 @@ export default class App extends React.Component {
 
 
   startCount() {
-    console.log('startCount workMinutes:', document.getElementById("workMinutes").value)
-    console.log('type workMinutes:', typeof(document.getElementById("workMinutes").value))
-    console.log('startCount pauseMinutes:', document.getElementById("pauseMinutes").value)
-    console.log('type pauseMinutes:', typeof(document.getElementById("pauseMinutes").value))
-    // console.log('this.myui', this.myUi)
     //check if timer is running
     if (workActive === false) {
       console.log('workactive in startcount == false')
       let countTime = 99
       if (this.state.count === 0 && workActive === false) {
         console.log('if count === 0 && !workActive?')
-
-
-        // if (isNaN(this.state.workMinutes) || this.state.workMinutes <= 0) {
-        //   console.log('if not 1: this.state.count', this.state.count)
-        //   console.log('if  not?')
-        //   // (() => this.setTimer(25))
-
-        //   // {() => this.setState({workMinutes: 25})}
-
-        //   console.log('if not 2: this.state.count', this.state.count)
-        // }
-        // let pauseMinutes = document.getElementById("pauseMinutes").value
         console.log('workminutes input', this.state.workMinutes)
         console.log('pauseminutes input', this.state.pauseMinutes)
         if (this.state.workTime) {
-          // if (isNaN(Number(this.state.workMinutes)) || this.state.workMinutes <= 0) {
-          if (isNaN(document.getElementById("workMinutes").value) || document.getElementById("workMinutes").value <= 0) {
-          // if (document.getElementById("workMinutes").value <= 0) {
+          if (isNaN(this.state.workMinutes) || this.state.workMinutes <= 0) {
             countTime = 25 * 60
           } else {
-
-            countTime = document.getElementById("workMinutes").value
-            // countTime = this.myUi
-            // countTime = this.state.workMinutes
+            countTime = (this.state.workMinutes * 60)
           }
         } else {
-          // if (isNaN(this.state.pauseMinutes) || this.state.pauseMinutes <= 0) {
-          if (isNaN(document.getElementById("pauseMinutes").value) || document.getElementById("pauseMinutes").value <= 0) {
+          if (isNaN(this.state.pauseMinutes) || this.state.pauseMinutes <= 0) {
             countTime = 5 * 60
           } else {
-            countTime = document.getElementById("pauseMinutes").value
-            // countTime = this.state.pauseMinutes
+            countTime = (this.state.pauseMinutes * 60)
           }
         }
         this.setState({
@@ -109,30 +74,37 @@ export default class App extends React.Component {
       }
       workActive = true
       console.log('this.state.count', this.state.count)
-      // let countText = document.getElementById("count")
-      // let newCount = this.state.count - 1 
-
       countInterval = setInterval(() => {
         this.countDown()
-
       }, 1000)
       console.log('end of startcount')
     }
   }
+
+
   stopCount() {
     console.log('stopCount')
     workActive = false
-    let startButton = document.getElementById("startButton")
-    // startButton.disabled = false
     clearInterval(countInterval)
   }
+
+
   resetTimer() {
     console.log('reset1: this.state.count', this.state.count)
-
+    if (isNaN(this.state.workMinutes) || this.state.workMinutes <= 0) {
+    this.setState({
+      workTime: true,
+      count: (25*60),
+    })
+    } else {
+      this.setState({
+        workTime: true,
+        count: (this.state.workMinutes * 60),
+      })
+    }
     this.stopCount()
     workActive = false
     console.log('reset 2: this.state.count', this.state.count)
-
   }
 
   render() {
@@ -140,30 +112,27 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text style={styles.big}>Pomodoro Timer</Text>
         <View style={styles.rowContainer}>
-          <Text>{this.state.workTime ? "Work time: " : "Rest time:"}</Text>
-          <Text id="count"> {this.minutesLeft()} : {this.secondsLeft()} </Text>
+          <Text >{this.state.workTime ? "Work time: " : "Rest time:"}</Text>
+          <Text id="count" style={styles.timer}> {this.minutesLeft()} : {this.secondsLeft()} </Text>
         </View>
         <ScrollView>
           <View style={styles.rowContainer}>
             <Text>Work time: </Text>
-            <TextInput id="workMinutes" placeholder='25' style={styles.input} ref={this.myUi}></TextInput>
-            {/* <TextInput id="workMinutes" placeholder='25' style={styles.input} onChangeText={(work) => {
-              this.setState({ workMinutes })
-            }}></TextInput> */}
+            <TextInput id="workMinutes" placeholder='25' style={styles.input} onChangeText={(workMinutes) => this.setState({ workMinutes })}
+            ></TextInput>
           </View>
           <View style={styles.rowContainer}>
             <Text>Pause time: </Text>
-            <TextInput id="pauseMinutes" placeholder='5' style={styles.input}></TextInput>
+            <TextInput id="pauseMinutes" placeholder='5' style={styles.input} onChangeText={(pauseMinutes) => this.setState({ pauseMinutes })}
+            ></TextInput>
           </View>
           <View style={styles.rowContainer}>
             <Button id="startButton" title="start" onPress={() => this.startCount()} style={styles.button} />
+            <Text>   </Text>
             <Button id="stopButton" title="stop" onPress={() => this.stopCount()} style={styles.button} />
+            <Text>   </Text>
             <Button id="resetButton" title="reset" onPress={() => this.resetTimer()} style={styles.button} />
           </View>
-          <Text> test</Text>
-          <Text>I Love Lulu!</Text>
-          <Switch checked={true}></Switch>
-
         </ScrollView>
       </View>
     );
@@ -192,15 +161,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   input: {
-    padding: 10,
-    height: 20,
+    margin: 10,
+    height: 50,
     width: 100,
     borderColor: 'gray',
     borderWidth: 1,
   },
   button: {
-    padding: 40,
+    margin: 202,
+    padding: 400,
     fontSize: 20,
     borderWidth: 1,
+  },
+  timer: {
+    alignSelf: 'stretch',
+    padding: 0,
+    fontSize: 40,
+
   }
 });
